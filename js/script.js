ed22b7c1f1988ba190b3a2badada57e7d3f29aab
Brian Dutz <bgdutz@gmail.com>
2016-08-26 08:51:04 -0700

    add basic back end framework and front end without input method implemented yet

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//business logic
+
+function Pizza(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+};
+
+Pizza.prototype.price = function(){
+  var price = 10
+  if (this.toppings <  ){
+      price = price += 1
+    } else if (this.size < 5) {
+      price = price * .75
+    } else {
+      price = price
+    }
+    return price;
+};
+
+//front end logic
+
+$(document).ready(function() {
+  $("#newPizza").submit(function(event) {
+    event.preventDefault();
+    var inputtedToppings = $("input#newToppings").val();
+    var inputtedSize = $("input#newSize").val();
+
+    var myPizza = new Pizza(inputtedToppings, inputtedSize);
+    console.log(myPizza);
+    $("#showPrice").append("<li>" + myPizza.price() + "</li>");
+  });
+});
